3ca46dcc7a23c19ed3fb03fab760f5e6b52d0ba7
Gaurav Mishra <grv20.mishra@gmail.com>
2016-12-03 21:20:51 -0500

    Changes to correct the apriori algo

--- src/AprioriTest.java
+++ src/AprioriTest.java
@@ -1,298 +1,208 @@
-import java.io.BufferedReader;
-
 import java.io.FileReader;
-
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
-import java.util.TreeMap;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 
 public class AprioriTest {
 
-	static List<List<String>> transactionList = new ArrayList<List<String>>();
+	static List<Set<String>> transactionList = new ArrayList<>();
 	static List<Rule> finalrules = new ArrayList<Rule>();
 	static List<Set<String>> largeItemList = new ArrayList<Set<String>>();
-	
-	public static void AprioriGen(String dataSet,double minSupport,double minConfidence) {
+	static Map<Set<String>, Double> supports = new HashMap<>();
+	static Set<Set<String>> set1 = new HashSet<>();
+
+	public static void AprioriGen(String dataSet, double minSupport,
+			double minConfidence) {
+
+		CSVParser csvFileParser = null;
+		CSVFormat csvFileFormat = CSVFormat.DEFAULT;
 
 		System.out.println("Start processing file");
-		List<Set<Set<String>>> largeItemSetList = new ArrayList<Set<Set<String>>>();
-		
-		Set<String> itemSet = new HashSet<String>();
-		Map<String,Integer> itemSupportMap = new HashMap<String,Integer>();
-		Map<Set<String>,Integer> largeItemSupportMap = new HashMap<Set<String>,Integer>();
-		
-		try
-		{
-			String fileName = "Resources/New_York_City_Leading_Causes_of_Death.csv";//+ dataSet + ".csv";
-			BufferedReader br = new BufferedReader(new FileReader(fileName));
-			String line;
-			while((line = br.readLine()) != null)
-			{
-				String[] items = line.split(",");
-				List<String> transaction = new ArrayList<String>();
-				for(String i : items)
-				{
-					if(itemSupportMap.containsKey(i))
-					{
-						int newCount = (itemSupportMap.get(i))+1;
-						itemSupportMap.remove(i);
-						itemSupportMap.put(i, newCount);
-						itemSet.add(i);
-					}
-					else
-					{
-						itemSupportMap.put(i, 1);
-					}
-					transaction.add(i);
+		List<List<List<String>>> largeItemSetList = new ArrayList<>();
+
+		Set<String> itemSet = new HashSet<>();
+		Map<String, Integer> itemSupportMap = new HashMap<>();
+		String fileName = "Resources/Inspections.csv";
+
+		try (FileReader fileReader = new FileReader(fileName)) {
+			csvFileParser = new CSVParser(fileReader, csvFileFormat);
+			List<CSVRecord> csvRecords = csvFileParser.getRecords();
+
+			for (int i = 0; i < csvRecords.size(); i++) {
+				CSVRecord record = csvRecords.get(i);
+				Iterator<String> itero = record.iterator();
+				Set<String> transaction = new HashSet<>();
+				while (itero.hasNext()) {
+					String item = "'" + itero.next() + "'";
+					itemSupportMap.put(item,
+							itemSupportMap.getOrDefault(item, 0) + 1);
+					itemSet.add(item);
+					transaction.add(item);
 				}
 				transactionList.add(transaction);
+
 			}
-			br.close();
+
 			System.out.println("Start 1st set ");
-			Iterator<Entry<String, Integer>> iterator = itemSupportMap.entrySet().iterator();
-			Set<String> temp = new HashSet<String>();
-			while(iterator.hasNext())
-			{
-				Entry<String, Integer> entry = iterator.next();
-				String key = (String)entry.getKey();
-				Integer value = (Integer)entry.getValue();
-				if((double)(value)/transactionList.size() >= minSupport)
-				{
-					temp.add(key);
+			List<List<String>> level1 = new ArrayList<>();
+			for (Map.Entry<String, Integer> entry : itemSupportMap.entrySet()) {
+				String key = entry.getKey();
+				Integer value = entry.getValue();
+				if ((double) (value) / transactionList.size() >= minSupport) {
+					List<String> list = new ArrayList<>();
+					list.add(key);
+					level1.add(list);
+					supports.put(new HashSet<>(list), (double) (value)
+							/ transactionList.size());
 				}
 			}
-			Set<Set<String>> setString = new HashSet<Set<String>>();
-			setString.add(temp);
-			largeItemSetList.add(setString);
+
+			largeItemSetList.add(level1);
 			System.out.println("Start remaining sets");
 			int k = 1;
-			while(largeItemSetList.get(k-1).size() != 0)
-			{
-				System.out.println("Large item set k:"+k);
-				Set<Set<String>> addToLargeItemSetList = new HashSet<Set<String>>(); //APRIORI-GEN---new candidates
-				ArrayList<String> entryList = new ArrayList<String>();
-				ArrayList<Set<String>> preCkList = new ArrayList<Set<String>>();
-				ArrayList<Set<String>> ckList = new ArrayList<Set<String>>();
 
-				Iterator<Set<String>> iterator3 = largeItemSetList.get(k-1).iterator();
-				while(iterator3.hasNext())
-				{
-					Set<String> s = iterator3.next();
-					for(String string : s)
-					{
-						entryList.add(string);
+			while (largeItemSetList.get(k - 1).size() != 0) {
+				List<List<String>> candidateLists = new ArrayList<>();
+				for (int i = 0; i < largeItemSetList.get(k - 1).size(); i++) {
+					for (int j = i + 1; j < largeItemSetList.get(k - 1).size(); j++) {
+						List<String> list1 = largeItemSetList.get(k - 1).get(i);
+						List<String> list2 = largeItemSetList.get(k - 1).get(j);
+						if (equalAllButOneElements(list1, list2)) {
+							if (!list1.get(list1.size() - 1).equals(
+									list2.get(list2.size() - 1))) {
+								List<String> newList = new ArrayList<>(list1);
+								newList.add(list2.get(list2.size() - 1));
+								Collections.sort(newList);
+								candidateLists.add(newList);
+							}
+						}
+
 					}
 				}
-				System.out.println("Checkpoint1 "+k);
-				preCkList = getSubsets(entryList);
-				
-				//pruning candidates not included in previous level-- could be done in getsubsets
-				for(int i = 0; i < preCkList.size(); i ++)
-				{
-					if(preCkList.get(i).size() == k+1)
-					{
-						ckList.add(preCkList.get(i));
-					}
-				}
-				System.out.println("Checkpoint2 "+k);
+				System.out.println("K: " + k);
 
-				for(int i = 0; i < ckList.size(); i ++)
-				{
+				List<List<String>> largeCandidates = calculateLargeList(
+						candidateLists, transactionList, minSupport);
 
-					if(!largeItemSetList.get(k-1).containsAll(ckList.get(i)))
-					{
-						ckList.remove(i);
-						continue;
-					}
-				}
-				System.out.println("Checkpoint "+k);
+				largeItemSetList.add(largeCandidates);
 
+				k++;
 
-				for(int i = 0; i < ckList.size(); i ++)
-				{
-					int count = 0;
-					for(int j = 0; j < transactionList.size(); j ++)
-					{
-						if(transactionList.get(j).containsAll(ckList.get(i)))
-							count ++;
-					}
-					if((double)((double)count/transactionList.size()) > minSupport)
-					{
-						addToLargeItemSetList.add(ckList.get(i));
-					}
-				}
-				k ++;
-				largeItemSetList.add(addToLargeItemSetList);
 			}
+
 			System.out.println("LargeItem Sets extraction done");
-			largeItemSetList.remove(largeItemSetList.size()-1); //Since last contains null
-			Set<Set<String>> tempSet = largeItemSetList.get(0);
-			Iterator<Set<String>> tempIter = tempSet.iterator();
-			Set<String> tempTempSet = tempIter.next();
-			Iterator<String> tempTempIter = tempTempSet.iterator();
-			Map<String, Integer> refMap = new HashMap<String, Integer>();
-			while(tempTempIter.hasNext())
-			{
-				String enterStringInSet = tempTempIter.next();
-				Set<String> newEntry = new HashSet<String>();
-				newEntry.add(enterStringInSet);
-				Integer newEntryCount = -1;
-				
-				Iterator<Entry<String, Integer>> innerIter = itemSupportMap.entrySet().iterator();
-				while(innerIter.hasNext())
-				{
-					Entry<String, Integer> entry = innerIter.next();
-					String key = (String)entry.getKey();
-					Integer value = (Integer)entry.getValue();
-					if(key.equals(enterStringInSet))
-					{
-						newEntryCount = value;
-						refMap.put(key, value);
-					}
+
+			largeItemSetList.remove(largeItemSetList.size() - 1); // Since last
+																	// contains
+																	// null
+
+			for (int i = 1; i < largeItemSetList.size(); i++) {
+
+				for (int j = 0; j < largeItemSetList.get(i).size(); j++) {
+					List<String> list = largeItemSetList.get(i).get(j);
+					partition(list, list, 0, new ArrayList<>(), minConfidence,
+							supports.get(new HashSet<>(list)));
 				}
-				largeItemSupportMap.put(newEntry, newEntryCount);
+
 			}
-			
-			for(int i = 1; i < largeItemSetList.size(); i ++)
-			{
-				Set<Set<String>> setSetString = largeItemSetList.get(i);
-				Iterator<Set<String>> innerIter = setSetString.iterator();
-				while(innerIter.hasNext())
-				{
-					Set<String> keySet = innerIter.next();
-					Integer supportValue = 0;
-					supportValue = getSupportCount(keySet);
-					largeItemSupportMap.put(keySet, supportValue);
-				}
-			}
-			System.out.println("Start rules processing");
-			
-			List<Rule> tempRuleList = new ArrayList<Rule>();
-			Iterator<Entry<Set<String>, Integer>> iter = largeItemSupportMap.entrySet().iterator();
-			while(iter.hasNext())
-			{
-				Entry<Set<String>, Integer> entry = iter.next();
-				Set<String> key = (Set<String>)entry.getKey();
-				Integer value = (Integer)entry.getValue();
-				Iterator<Entry<Set<String>, Integer>> inneriter = largeItemSupportMap.entrySet().iterator();
-				while(inneriter.hasNext())
-				{
-					Entry<Set<String>, Integer> innerEntry = inneriter.next();
-					Set<String> innerKey = (Set<String>)innerEntry.getKey();
-					Integer innerValue = (Integer)innerEntry.getValue();
-					
-					if(union(key, innerKey).size() != 0 && innerKey.size() == 1)
-					{
-						Rule rule = new Rule(transactionList);
-						rule.setLhs(key);
-						rule.setRhs(innerKey);
-						rule.setLhsCount(value);
-						rule.setRhsCount(innerValue);
-						tempRuleList.add(rule);
-					}
-				}
-			}
-			
-			
-			for(int i = 0; i < tempRuleList.size(); i ++)
-			{
-				Rule currentRule = tempRuleList.get(i);
-				if(currentRule.getLeft().containsAll(currentRule.getRight()))
-				{
-					tempRuleList.remove(currentRule);
-					continue;
-				}
-				if(currentRule.calculateConfidence() < minConfidence)
-				{
-					tempRuleList.remove(currentRule);
-					continue;
-				}
-				finalrules.add(currentRule);
-			}
-			SetComparator comp =  new SetComparator(largeItemSupportMap);
-			TreeMap<Set<String>, Integer> sortedSupportMap = new TreeMap<Set<String>, Integer>(comp);
-			sortedSupportMap.putAll(largeItemSupportMap);
-			
-			Comparator<Rule> comparator = new Comparator<Rule>() {
-				public int compare(Rule r1, Rule r2) {
-					if(r1.confidence <= r2.confidence)
-						return 1;
-					return -1;
-				}
-			}; 
+
 			System.out.println("Rule processing complete");
-			Collections.sort(finalrules, comparator);
-			
-			
-			for(int i = 0; i < finalrules.size(); i ++)
-			{
+			Collections.sort(finalrules);
+
+			for (int i = 0; i < finalrules.size(); i++) {
 				System.out.println(finalrules.get(i));
 			}
-			
+
 		}
-		
-		catch(Exception e)
-		{
+
+		catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	private static ArrayList<Set<String>> getSubsets(ArrayList<String> set)
-	{
 
-		ArrayList<Set<String>> subsetCollection = new ArrayList<Set<String>>();
+	private static List<List<String>> calculateLargeList(
+			List<List<String>> candidateLists,
+			List<Set<String>> transactionList2, double minSupport) {
 
-		if (set.size() == 0) {
-			subsetCollection.add(new HashSet<String>());
-		} else {
-			ArrayList<String> reducedSet = new ArrayList<String>();
+		List<List<String>> largeData = new ArrayList<>();
+		Map<List<String>, Integer> map = new HashMap<>();
+		for (Set<String> transaction : transactionList2) {
 
-			reducedSet.addAll(set);
-
-			String first = reducedSet.remove(0);
-			ArrayList<Set<String>> subsets = getSubsets(reducedSet);
-			subsetCollection.addAll(subsets);
-
-			subsets = getSubsets(reducedSet);
-
-			for (Set<String> subset : subsets) {
-				subset.add(first);
+			for (List<String> list : candidateLists) {
+				Set<String> set = new HashSet<>(list);
+				if (transaction.containsAll(set)) {
+					map.put(list, map.getOrDefault(list, 0) + 1);
+				}
 			}
 
-			subsetCollection.addAll(subsets);
 		}
 
-		return subsetCollection;
-	}
-	
-	public static <T>Set<T> union(Set<T> setA, Set<T> setB) {
-	    Set<T> tmp = new HashSet<T>(setA);
-	    tmp.addAll(setB);
-	    return tmp;
-	  }
-	
-	private static int getSupportCount(Set<String> set)
-	{
-		int count = 0;
-		for(int i = 0; i < transactionList.size(); i ++)
-		{
-			if(transactionList.get(i).containsAll(set))
-			{
-				count ++;
+		for (Map.Entry<List<String>, Integer> entry : map.entrySet()) {
+			if ((double) (entry.getValue()) / transactionList.size() >= minSupport) {
+				List<String> list = entry.getKey();
+				largeData.add(list);
+				supports.put(new HashSet<>(list), (double) (entry.getValue())
+						/ transactionList.size());
 			}
 		}
-		return count;
+
+		return largeData;
 	}
-	public static void main(String args[]){
-		AprioriGen("Sample",0.07,0.01);
+
+	private static boolean equalAllButOneElements(List<String> list1,
+			List<String> list2) {
+		if (list1.size() != list2.size())
+			return false;
+		for (int i = 0; i < list1.size() - 1; i++) {
+			if (!list1.get(i).equals(list2.get(i)))
+				return false;
+		}
+		return true;
+	}
+
+	private static void partition(List<String> original, List<String> list1,
+			int start, List<String> list2, double minConfidence,
+			double supportNumerator) {
+		if (set1.contains(new HashSet<>(list1))) {
+			return;
+		}
+		if (!(list1.size() == 0) && !(list2.size() == 0)) {
+			Double supportDenominator = supports.get(new HashSet<>(list1));
+
+			double conf = supportNumerator / (double) supportDenominator;
+
+			if (conf >= minConfidence) {
+				Rule rule = new Rule(new HashSet<>(list1),
+						new HashSet<>(list2), conf, supportNumerator);
+				finalrules.add(rule);
+			}
+
+			set1.add(new HashSet<>(list1));
+
+		}
+
+		for (int i = 0; i < list1.size(); i++) {
+			String temp = list1.get(i);
+			list2.add(temp);
+			list1.remove(i);
+			partition(original, list1, i + 1, new ArrayList<>(list2),
+					minConfidence, supportNumerator);
+			list1.add(i, temp);
+			list2.remove(list2.size() - 1);
+		}
+
+	}
+
+	public static void main(String args[]) {
+		AprioriGen("Sample", 0.01, 0.5);
 	}
 }
--- src/Rule.java
+++ src/Rule.java
@@ -1,92 +1,32 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
+public class Rule implements Comparable<Rule> {
 
-public class Rule {
-	
 	Set<String> left;
-	int leftSup;
-	Set<String> right;	
-	int rightSup;
+	Set<String> right;
 	double confidence;
 	double support;
-	
-List<List<String>> transactionList = new ArrayList<List<String>>();
-	
-	Rule(List<List<String>> tl)
-	{
-		transactionList = tl;
-		left = new HashSet<String>();
-		right = new HashSet<String>();
-	}
-	
-	private int calculateSupport(Set<String> set)
-	{
-		int c = 0;
-		for(int i = 0; i < transactionList.size(); i ++)
-		{
-			if(transactionList.get(i).containsAll(set))
-			{
-				c ++;
-			}
-		}
-		return c;
-	}
-	
-	public void setLhs(Set<String> left) {
+
+	public Rule(Set<String> left, Set<String> right, double confidence,
+			double support) {
 		this.left = left;
+		this.right = right;
+		this.confidence = confidence;
+		this.support = support;
 	}
-	
-	public Set<String> getLeft() {
-		return left;
-	}
-	
+
 	@Override
 	public String toString() {
-		return Arrays.toString(left.toArray()) + " ==> " + Arrays.toString(right.toArray()) + " (Confidence: " + confidence * 100 + "%, Support: " + (calculateSupport(union(left,right)) / (double)transactionList.size()) * 100 + "%)";
+		return Arrays.toString(left.toArray()) + " ==> "
+				+ Arrays.toString(right.toArray()) + " (Confidence: "
+				+ confidence * 100 + "%, Support: " + support + "%)";
 	}
-	
-	public int getLeftCount() {
-		return leftSup;
+
+	@Override
+	public int compareTo(Rule that) {
+		return -1*(this.confidence < that.confidence ? -1
+				: this.confidence > that.confidence ? 1 : 0);
 	}
-	
-	public Set<String> getRight() {
-		return right;
-	}
-	
-	public int getRightSup() {
-		return rightSup;
-	}
-	
-	public void setLhsCount(int leftSupport) {
-		this.leftSup = leftSupport;
-	}
-	
-	public void setRhs(Set<String> right) {
-		this.right = right;
-	}
-	
-	public void setRhsCount(int rightSupport) {
-		this.rightSup = rightSupport;
-	}
-	
-	public double calculateConfidence()
-	{
-		confidence = calculateSupport(union(left, right));
-		support = (int)confidence;
-		confidence /= calculateSupport(left);
-		return confidence;
-	}
-	
-	public <T>Set<T> union(Set<T> setA, Set<T> setB) {
-	    Set<T> tmp = new HashSet<T>(setA);
-	    tmp.addAll(setB);
-	    return tmp;
-	  }
-	
 
 }
